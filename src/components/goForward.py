326fbace02c69f422c9fa1ccd5c9a43ad2a72068
olliverpersson <48672712+olliverpersson@users.noreply.github.com>
2020-02-27 08:40:03 +0100

    first verison of goForward

--- src/components/goForward.py
+++ src/components/goForward.py
@@ -1,2 +1,14 @@
-from components.gpios import MVOO, MVPLUS, MVMINUS, MHOO, MHPLUS, MHMINUS, MK, SV, SH
+from components.gpios import MVOO, MVPLUS, MVMINUS, MHOO, MHPLUS, MHMINUS, MKOO
 
+def goForward():
+  
+  MVOO.off()
+  MHOO.off()
+
+  MVPLUS.off()
+  MVMINUS.off()
+  MHPLUS.off()
+  MHMINUS.off()
+
+  MVOO.on()
+  MHOO.on()
